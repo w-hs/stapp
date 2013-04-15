f7cffd40e97cdccc496dd54df085ba0a83e9c925
chris.schluetter@gmail.com <chris.schluetter@gmail.com@df7c1e32-7cd3-0938-7b03-7a48d7c20f59>
2013-04-15 15:57:44 +0000

    POJOS angepasst. Membervariablen protected gemacht..

--- trunk/src/de/whs/stapp/database/TrainingUnitDetail.java
+++ trunk/src/de/whs/stapp/database/TrainingUnitDetail.java
@@ -7,11 +7,17 @@ import de.whs.stapp.liveDataTracking.TrackedDataItem;
 /**
  * 
  * @author Christoph Inhestern
- *Enthält alle Daten einer Trainingseinheit, inklusive einer Liste
- *über alle Messwerten.
+ * Enthält alle Daten einer Trainingseinheit, inklusive einer Liste
+ * über alle Messwerten.
  */
 public class TrainingUnitDetail extends TrainingUnit{
-
 	
-	public ArrayList<TrackedDataItem> trackedDataCollection = new ArrayList<TrackedDataItem>();
+	protected ArrayList<TrackedDataItem> trackedDataCollection = new ArrayList<TrackedDataItem>();
+
+	/**
+	 * @return the trackedDataCollection
+	 */
+	public ArrayList<TrackedDataItem> getTrackedDataCollection() {
+		return trackedDataCollection;
+	}
 }
