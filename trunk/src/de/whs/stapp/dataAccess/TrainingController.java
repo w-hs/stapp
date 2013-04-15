ee2832f9509f19876a5f2999ddd925c0b26dce4f
chris.schluetter@gmail.com <chris.schluetter@gmail.com@df7c1e32-7cd3-0938-7b03-7a48d7c20f59>
2013-04-15 21:34:48 +0000

    Kommentare angepasst und Logik implementiert.

--- trunk/src/de/whs/stapp/dataAccess/TrainingController.java
+++ trunk/src/de/whs/stapp/dataAccess/TrainingController.java
@@ -1,32 +1,11 @@
 package de.whs.stapp.dataAccess;
 
 import de.whs.stapp.database.DatabaseAdapter;
+import de.whs.stapp.database.TrainingUnit;
 import de.whs.stapp.liveDataTracking.BTServiceConnectionRegisterable;
 import de.whs.stapp.liveDataTracking.TrackedDataEvent;
 import de.whs.stapp.liveDataTracking.TrackedDataItem;
 import de.whs.stapp.liveDataTracking.TrackedDataListener;
-
-// TrainingController hat zwei Abhängigkeiten:
-// - "Irgendwas" an dem ich mich registrieren kann
-// - Zugriff auf Datenbank...
-
-
-/*
- * 		TrackedDataListener trackedDataListener = new TrackedDataListener() {
-			@Override
-			public void getTrackedData(TrackedDataEvent e) {
-				//TrackedDataItem item = e.getDataItem();	
-				
-				if (state != TrainingState.RUNNING)
-					return;
-				
-//				notifyOthers(this, e);
-//				persistDataToChristoph(unitDetails.id, item);	
-			}
-		};
-		
- */
-
 
 /**
  * Verwaltet eine Trainingseinheit.
@@ -35,28 +14,80 @@ import de.whs.stapp.liveDataTracking.TrackedDataListener;
 public class TrainingController {
 
 	private TrainingState state;
+	private TrainingUnit trainingUnit;
 	
 	// Für den Zugriff auf die Bluetooth "Tracking" Events...
 	// Ggf. eine bessere Implementierung möglich?!
 	private BTServiceConnectionRegisterable serviceConnection;
 	private DatabaseAdapter databaseAdapter;
-		
+			
 	
 	/**
-	 *  Erzeugt eine neue Instanz der {@link TrainingController} Klasse.
+	 * @return Die aktuelle {@link TrainingUnit}.
+	 */
+	public TrainingUnit getTrainingUnit() { return trainingUnit; }
+	
+	
+	/**
+	 * Erzeugt eine neue Instanz der {@link TrainingController} Klasse.
 	 * @param databaseAdapter Eine {@link DatabaseAdapter} Implementierung.
 	 * @param serviceConnection Eine {@link BTServiceConnectionRegisterable} Implementierung.
 	 */
 	public TrainingController(DatabaseAdapter databaseAdapter, 
-			BTServiceConnectionRegisterable serviceConnection) {
+							  BTServiceConnectionRegisterable serviceConnection) {
 		
 		this.databaseAdapter = databaseAdapter;
 		this.serviceConnection = serviceConnection;		
+		
 		state = TrainingState.NEW;
+		trainingUnit = new TrainingUnit();
+	}
+	
+	/**
+	 * Liefert den aktuellen Status der Trainingseinheit.
+	 * @return Der aktuelle Trainingsstatus.
+	 */
+	public TrainingState getState() {
+		return state;
+	}
+	
+	/**
+	 * Startet die aktuelle Trainingseinheit.
+	 */
+	public void start() {
+		if (state != TrainingState.NEW) return;
+		
+		state = TrainingState.RUNNING;
+		trainingUnit = databaseAdapter.createNewTrainingUnit();
 		
 		registerTrackedDataListener();
 	}
 	
+	/**
+	 * Beendet die aktuelle Trainingseinheit.
+	 */
+	public void stop() {
+		if (state == TrainingState.FINISHED) return;
+		state = TrainingState.FINISHED;
+	}
+	
+//	/**
+//	 * @return Liefert den insgesamt gespeicherten Datensatz.
+//	 */
+//	public TrainingUnitDetail getStoredData() {
+//		// TODO, Christoph, Chris: Prüfen ob der Datensatz mit der Id noch existiert,
+//		// da er ja über removeStoredData bereits gelöscht worden sein könnte...
+//		return databaseAdapter.getTrainingUnitDetail(trainingUnit.getSessionID());		
+//	}
+//	
+//	/**
+//	 * Verwerfe diese Trainingseinheit wieder.
+//	 */
+//	public void removeStoredData() {
+//		// TODO Christoph, Chris: Methode zum Löschen programmieren.
+//	}
+	
+
 	private void registerTrackedDataListener() {
 		serviceConnection.registerListener(new TrackedDataListener() {
 			@Override
@@ -67,47 +98,9 @@ public class TrainingController {
 					return;
 				
 //				notifyOthers(this, e);
-//				databaseAdapter.saveTrackedDataItem(trainingUnitId, item);
+				databaseAdapter.saveTrackedDataItem(trainingUnit.getSessionID(), item);
 			}
 		});
-		
-	}
-
-	/**
-	 * Liefert den aktuellen Trainingsstatus zurück.
-	 * @return Der aktuelle Trainingsstatus.
-	 */
-	public TrainingState getState() {
-		return state;
-	}
-	
-	/**
-	 * .
-	 */
-	public void start() {
-		// Sollte die Trainingseinheit mit Details zurück geben?!
-		throw new UnsupportedOperationException("start() is not implemented yet.");
-	}
-	
-//	/**
-//	 * .
-//	 */
-//	public void pause() {
-//		throw new UnsupportedOperationException("pause() is not implemented yet.");
-//	}
-//	
-//	/**
-//	 * .
-//	 */
-//	public void proceed() {
-//		throw new UnsupportedOperationException("proceed() is not implemented yet.");
-//	}
-	
-	/**
-	 * .
-	 */
-	public void stop() {
-		throw new UnsupportedOperationException("stop() is not implemented yet.");
 	}
 	
 	// Dadurch das wir die ganze Zeit speichern wird save 
@@ -118,4 +111,12 @@ public class TrainingController {
 	// Datenbank gelöscht werden, sofern man die Trainingseinheit
 	// nicht speichern sondern wieder verwerfen möchte.
 	// public void forget();
+	
+	//public void pause() {
+	//	throw new UnsupportedOperationException("pause() is not implemented yet.");
+	//}
+	//
+	//public void proceed() {
+	//	throw new UnsupportedOperationException("proceed() is not implemented yet.");
+	//}
 }
