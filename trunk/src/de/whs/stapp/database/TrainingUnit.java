9ed87f92886e88eee370c1ecc358c824ea3eafd9
c.inhestern@gmail.com <c.inhestern@gmail.com@df7c1e32-7cd3-0938-7b03-7a48d7c20f59>
2013-04-15 15:41:23 +0000

    #37 - Schnittstellendefinition der Methoden zum Zugriff auf die Datenbank. #38 - Datenmodell erstellt und Klassen angelegt.

---
+++ trunk/src/de/whs/stapp/database/TrainingUnit.java
@@ -0,0 +1,18 @@
+package de.whs.stapp.database;
+
+
+/**
+ * 
+ * @author Christoph Inhestern
+ * Class to represent the Data from one Training session.
+ */
+public class TrainingUnit{
+	public int sessionID;	
+	public int distance;
+	public long date;
+
+}
+
+
+//Andere Möglichkeit wäre in einem Training alle werte aufzunehmen 
+//und mit einer Liste dieser zu arbeiten. sessionID dann redundant
