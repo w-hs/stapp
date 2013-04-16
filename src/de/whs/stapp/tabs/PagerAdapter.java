6b0cfdd02a82c6b358db1a92f7ed0b62f6606686
danielhardes@gmail.com <danielhardes@gmail.com@df7c1e32-7cd3-0938-7b03-7a48d7c20f59>
2013-04-16 13:01:19 +0000

    #31 Training und Verlauf als Views. Navigation über Swipe. #32 Realisierung eines Menüs. #33 Von View abhängige Buttons zur Interaktion mit Webview.

---
+++ src/de/whs/stapp/tabs/PagerAdapter.java
@@ -0,0 +1,47 @@
+package de.whs.stapp.tabs;
+
+import java.util.List;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+ 
+/**
+ * Page Adapter wird benötigt um die Tab Fragments (Seite der Tabs) zu handhaben.
+ * 
+ * @author Daniel
+ *
+ */
+public class PagerAdapter extends FragmentPagerAdapter {
+ 
+    private List<Fragment> fragments;
+   
+    /**
+     * 
+     * @param fm Manager der Tab Fragments
+     * @param fragments liste alles Tab Fragments
+     */
+    public PagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    /**
+     * @param position Nummer des angeforderten tabs
+     * @return Das angeforderte Fragment/Tabs
+     * 
+     */
+    @Override
+    public Fragment getItem(int position) {
+        return this.fragments.get(position);
+    }
+ 
+    
+    /**
+     * @return Anzahl der Fragmente/Tabs
+     */
+    @Override
+    public int getCount() {
+        return this.fragments.size();
+    }
+}
---
+++ src/de/whs/stapp/tabs/TabFactory.java
@@ -0,0 +1,33 @@
+package de.whs.stapp.tabs;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.TabHost.TabContentFactory;
+
+/**
+ * Einfache Factory die views zu den Tabhost gibt.
+ * @author Daniel
+ */
+public class TabFactory implements TabContentFactory {
+	  private final Context mContext;
+	  
+      /**
+       * Konstruktor der Factory.
+       * @param context Context
+       */
+      public TabFactory(Context context) {
+          mContext = context;
+      }
+
+      /**
+       * Erstellt View für die Tabs.
+       * 
+       * @param tag Beschreibung eines Tabs
+       */
+      public View createTabContent(String tag) {
+          View v = new View(mContext);
+          v.setMinimumWidth(0);
+          v.setMinimumHeight(0);
+          return v;
+      }
+}
