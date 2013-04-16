3ba81cadf629f94da0f0da60a8e667ff69436448
danielhardes@gmail.com <danielhardes@gmail.com@df7c1e32-7cd3-0938-7b03-7a48d7c20f59>
2013-04-16 13:01:19 +0000

    #31 Training und Verlauf als Views. Navigation über Swipe. #32 Realisierung eines Menüs. #33 Von View abhängige Buttons zur Interaktion mit Webview.

---
+++ trunk/src/de/whs/stapp/tabs/TabHistoryFragment.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package de.whs.stapp.tabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+
+import de.whs.stapp.R;
+import de.whs.stapp.presentation.TrainingseinheitWebView;
+
+/**
+ * Das History Fragment/Tab welches ein Web View über History Informationen beinhaltet.
+ * @author DanielW7
+ *
+ */
+public class TabHistoryFragment extends Fragment {
+
+	private TrainingseinheitWebView mTrainingseinheitWebview;
+	
+	/**
+	 * Konstruktor des Tabs in dem Das Layout geladen wird.
+	 * @param inflater inflater
+	 * @param container container
+	 * @param savedInstanceState savedInstanceState
+	 */
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		if (container == null) {
+            return null;
+        }
+		return (RelativeLayout)inflater.inflate(R.layout.tab_history_layout, container, false);
+	}
+	
+	/**
+	 * Wird aufgerufen wenn alle Layouts und Activitys geladen sind.
+	 * @param savedInstanceState Saved State
+	 */
+	@Override 
+	public void onActivityCreated(Bundle savedInstanceState) { 
+	    super.onActivityCreated(savedInstanceState);   
+	    
+	    
+	    mTrainingseinheitWebview = new TrainingseinheitWebView(this.getActivity());
+        RelativeLayout relativeLayout = 
+                        (RelativeLayout) this.getActivity().findViewById(R.id.WrapperHistory);
+        RelativeLayout.LayoutParams relParams = 
+                new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        
+        relativeLayout.addView(mTrainingseinheitWebview, relParams);
+	}
+
+}
---
+++ trunk/src/de/whs/stapp/tabs/TabTrainingseinheitFragment.java
@@ -0,0 +1,97 @@
+/**
+ * 
+ */
+package de.whs.stapp.tabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+
+import de.whs.stapp.R;
+import de.whs.stapp.presentation.TrainingseinheitWebView;
+
+/**
+ * Das Trainingseinheiten Fragment/Tab welches ein Web View über Trainingseinheiten Informationen beinhaltet.
+ * @author DanielW7
+ *
+ */
+public class TabTrainingseinheitFragment extends Fragment {
+	
+	private TrainingseinheitWebView mTrainingseinheitWebview;
+	private Button mbtnStart;
+	/**
+	 * Konstruktor des Tabs in dem Das Layout geladen wird.
+	 * @param inflater inflater
+	 * @param container container
+	 * @param savedInstanceState savedInstanceState
+	 */
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		if (container == null) {
+            return null;
+        }
+		return (RelativeLayout)inflater.inflate(R.layout.tab_trainingseinheit_layout, container, false);
+	}
+	
+	/**
+	 * Wird aufgerufen wenn alle Layouts und Activitys geladen sind.
+	 * @param savedInstanceState Saved State
+	 */
+	@Override 
+	public void onActivityCreated(Bundle savedInstanceState) { 
+	    super.onActivityCreated(savedInstanceState);   
+	    
+	    
+	    mTrainingseinheitWebview = new TrainingseinheitWebView(this.getActivity());
+        RelativeLayout relativeLayout = 
+                        (RelativeLayout) this.getActivity().findViewById(R.id.WrapperTrainingseinheit);
+        RelativeLayout.LayoutParams relParams = 
+                new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        relativeLayout.addView(mTrainingseinheitWebview, relParams);
+        
+        //Hinzufügen eines Test Buttons in den Footer
+        RelativeLayout footer = 
+                (RelativeLayout) this.getActivity().findViewById(R.id.footer);
+        
+        mbtnStart = new Button(getActivity());
+        
+        mbtnStart.setText("Start");
+        mbtnStart.setLayoutParams(new LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        footer.addView(mbtnStart);
+        
+        
+        //OnClick Event Handler zu Testzwecken. Kann später wieder entfernt werden.
+        mbtnStart.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				if(mTrainingseinheitWebview != null)
+					mTrainingseinheitWebview.startTraining();
+			}
+		});
+		 
+	}
+	
+	/**
+	 * macht alle buttons dieses Tab im Footer unsichtbar.
+	 */
+	public void hideButtons()
+	{
+		mbtnStart.setVisibility(View.GONE);
+	}
+	
+	/**
+	 * Zeiggt alle Buttons des Tabs im Footer wider an.
+	 */
+	public void showButtons()
+	{
+		mbtnStart.setVisibility(View.VISIBLE);
+	}
+}
