b184f115da210e1a84c74e2837024178652804bd
c.inhestern@gmail.com <c.inhestern@gmail.com@df7c1e32-7cd3-0938-7b03-7a48d7c20f59>
2013-04-18 14:43:54 +0000

    #37 Datenbankzugriff anlegen Die Klasse DatabaseConnector erstellt.

---
+++ trunk/src/de/whs/stapp/data/storage/DatabaseConnector.java
@@ -0,0 +1,56 @@
+package de.whs.stapp.data.storage;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * 
+ * @author Christoph
+ * 
+ * Die Klasse stellt eine Verbindung zur Datenbank her.
+ * Existiert die Datenbank nicht, so wird sie erstellt und die Methode onCreate() gerufen.
+ * 
+ */
+
+class DatabaseConnector extends SQLiteOpenHelper{
+
+	public final String tabTrainingUnits = "TrainingUnits";
+	public final String tuClmDate = "Date";
+	public final String tuClmDuration = "Duration";
+	public final String tuClmDistance = "Distance";
+	
+	public final String tabTrackedData = "TrackedData";
+	public final String tdClmIdTrainingUnit = "_idTrainingUnit";
+	public final String tdClmHeartrate = "Heartrate";
+	public final String tdClmDistance = "Distance";
+	public final String tdClmSpeed = "Speed";
+	public final String tdClmStrides = "Strides";
+	
+	
+	public DatabaseConnector(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO CREATE TABLE Anweisungen zum erstellen der Datenbank.
+		
+		String createTrainingUnits = "CREATE TABLE...";
+		String createTrackedData = "CREATE TABLE...";
+		
+		
+		db.execSQL(createTrainingUnits);
+		db.execSQL(createTrackedData);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Was soll beim erhöhen der DB Version passieren?
+		
+	}
+
+
+}
