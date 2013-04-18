029f9a4bfaeb8fa1522f040858b537581e7735a0
chris.schluetter@gmail.com <chris.schluetter@gmail.com@df7c1e32-7cd3-0938-7b03-7a48d7c20f59>
2013-04-18 10:08:00 +0000

    #45 "Interface für Datenzugriff entwickeln" Die Klassen DetailedTrainingUnit, TrainingUnit und TrainingDetail wurden angepasst und enthalten nun nur noch die relevanten Daten mit aussagekräftigen Bezeichner.
    Dazu waren ebenfalls Änderungen an StappDbAdapter und StappDbHelper nötig.

--- trunk/src/de/whs/stapp/data/storage/TrainingDetail.java
+++ trunk/src/de/whs/stapp/data/storage/TrainingDetail.java
@@ -1,7 +1,7 @@
 package de.whs.stapp.data.storage;
 
 /**
- * Details einer Trainingseinheit.
+ * Repräsentiert die Details einer Trainingseinheit.
  * @author Chris
  */
 public class TrainingDetail {
@@ -9,7 +9,22 @@ public class TrainingDetail {
 	private int heartRate;
 	private int distanceInMeter;
 	private int speedInMeterPerSecond;
+	private int numberOfStrides;
 	
+	/**
+	 * @return the numberOfStrides
+	 */
+	public int getNumberOfStrides() {
+		return numberOfStrides;
+	}
+
+	/**
+	 * @param numberOfStrides the numberOfStrides to set
+	 */
+	public void setNumberOfStrides(int numberOfStrides) {
+		this.numberOfStrides = numberOfStrides;
+	}
+
 	/**
 	 * Erstell eine neue Instanz der {@link TrainingDetail} Klasse.
 	 */
@@ -23,35 +38,39 @@ public class TrainingDetail {
 	public int getHeartRate() {
 		return heartRate;
 	}
+	
 	/**
 	 * @param heartRate the heartRate to set
 	 */
 	public void setHeartRate(int heartRate) {
 		this.heartRate = heartRate;
 	}
+	
 	/**
 	 * @return the distanceInMeter
 	 */
 	public int getDistanceInMeter() {
 		return distanceInMeter;
 	}
+	
 	/**
 	 * @param distanceInMeter the distanceInMeter to set
 	 */
 	public void setDistanceInMeter(int distanceInMeter) {
 		this.distanceInMeter = distanceInMeter;
 	}
+	
 	/**
 	 * @return the speedInMeterPerSecond
 	 */
 	public int getSpeedInMeterPerSecond() {
 		return speedInMeterPerSecond;
 	}
+	
 	/**
 	 * @param speedInMeterPerSecond the speedInMeterPerSecond to set
 	 */
 	public void setSpeedInMeterPerSecond(int speedInMeterPerSecond) {
 		this.speedInMeterPerSecond = speedInMeterPerSecond;
 	}
-	
 }
