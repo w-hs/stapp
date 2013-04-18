db750dbddc185175c2fe322e7f3f0cb700993ec9
chris.schluetter@gmail.com <chris.schluetter@gmail.com@df7c1e32-7cd3-0938-7b03-7a48d7c20f59>
2013-04-18 11:17:02 +0000

    #46 "Training Controller" erstellen.
    Der TrainingController hat nun folgende Möglichkeiten:
    - Trainingseinheit starten
    - Trainingseinheit beenden
    - Ein Listener registrieren, der über aktuelle TrainingDetails informiert wird (sekündlich)
    - Listener können wieder deregistriert werden
    - Den Status der Trainingseinheit abrufen
    - Die aktuelle TrainingUnit abrufen

--- trunk/src/de/whs/stapp/data/access/TrainingController.java
+++ trunk/src/de/whs/stapp/data/access/TrainingController.java
@@ -7,6 +7,7 @@ import de.whs.stapp.data.bluetooth.DataTracker;
 import de.whs.stapp.data.bluetooth.TrackedDataItem;
 import de.whs.stapp.data.bluetooth.TrackedDataListener;
 import de.whs.stapp.data.storage.DatabaseAdapter;
+import de.whs.stapp.data.storage.TrainingDetail;
 import de.whs.stapp.data.storage.TrainingUnit;
 
 /**
@@ -14,157 +15,130 @@ import de.whs.stapp.data.storage.TrainingUnit;
  * @author Chris
  */
 public class TrainingController {
-
-	private List<TrackedDataListener> trackedDataListeners =
-			new ArrayList<TrackedDataListener>();
-
-	private DataTracker serviceConnection;
-	private DatabaseAdapter databaseAdapter;
 	
-	private TrainingState state;
-	private TrainingUnit trainingUnit;
+	private DataTracker tracker;
+	private DatabaseAdapter database;	
+	private TrackedDataListener dataListener;
 
-	/**
-	 * @return Die aktuelle {@link TrainingUnit}.
-	 */
-	public TrainingUnit getTrainingUnit() {
-		return trainingUnit;
-	}
+	private TrainingState state = TrainingState.NEW;
+	private TrainingUnit currentTraining = new TrainingUnit();
+	
+	private List<TrainingDetailListener> trainingDetailListeners =
+			new ArrayList<TrainingDetailListener>();
+
 
 	/**
 	 * Erzeugt eine neue Instanz der {@link TrainingController} Klasse.
-	 * @param databaseAdapter Eine {@link DatabaseAdapter} Implementierung.
-	 * @param serviceConnection Eine {@link DataTracker} Implementierung.
+	 * @param tracker Eine {@link DataTracker} Implementierung.
+	 * @param database Eine {@link DatabaseAdapter} Implementierung.
 	 */
-	public TrainingController(DatabaseAdapter databaseAdapter,
-			DataTracker serviceConnection) {
+	public TrainingController(DataTracker tracker, DatabaseAdapter database) {		
+		if (database == null)
+			throw new IllegalArgumentException("databaseAdapter cannot be null");
+		if (tracker == null)
+			throw new IllegalArgumentException("serviceConnection cannot be null");
+
+		this.database = database;
+		this.tracker = tracker;
 		
-		if (databaseAdapter == null)
-			throw new IllegalArgumentException("databaseAdapter cannot be null.");
-		if (serviceConnection == null)
-			throw new IllegalArgumentException("serviceConnection cannot be null.");
-
-		this.databaseAdapter = databaseAdapter;
-		this.serviceConnection = serviceConnection;
-
-		state = TrainingState.NEW;
-		trainingUnit = new TrainingUnit();
+		initializeDataListener();
+	}
+	
+	/**
+	 * @return Die aktuelle {@link TrainingUnit}.
+	 */
+	public TrainingUnit getCurrentTrainingUnit() {
+		return currentTraining;
 	}
 
 	/**
-	 * Liefert den aktuellen Status der Trainingseinheit.
-	 * @return Der aktuelle Trainingsstatus.
+	 * @return Der Status der Trainingseinheit.
 	 */
 	public TrainingState getState() {
 		return state;
 	}
 
 	/**
-	 * Startet die aktuelle Trainingseinheit.
+	 * Startet die Trainingseinheit.
 	 */
 	public void start() {
 		if (state != TrainingState.NEW)
 			return;
 
 		state = TrainingState.RUNNING;
-		trainingUnit = databaseAdapter.createNewTrainingUnit();
+		currentTraining = database.createNewTrainingUnit();
 
-		listenToTrackedData();
+		tracker.registerListener(dataListener);
 	}
 
 	/**
 	 * Beendet die aktuelle Trainingseinheit.
 	 */
 	public void stop() {
-		if (state == TrainingState.FINISHED)
-			return;
-		state = TrainingState.FINISHED;
-		// TODO unregisterListener!
-	}
-
-	/**
-	 * Registriert einen listener, am "TrackedDataEvent", dass gefeuert wird,
-	 * wenn die aktuelle Trainingseinheit aktiv ist und Daten empfangen bzw.
-	 * protokolliert werden.
-	 * @param listener Der {@link TrackedDataListener}.
-	 */
-	public void registerTrackedDataListener(TrackedDataListener listener) {
-		if (listener == null)
-			throw new IllegalArgumentException("listener cannot be null");
-
-		if (!trackedDataListeners.contains(listener))
-			trackedDataListeners.add(listener);
-	}
-
-	/**
-	 * Entfernt einen registrierten listener vom "TrackedDataEvent".
-	 * @param listener Der {@link TrackedDataListener}.
-	 */
-	public void unregisterTrackedDataListener(TrackedDataListener listener) {
-		if (listener == null)
-			throw new IllegalArgumentException("listener cannot be null");
-
-		if (trackedDataListeners.contains(listener))
-			trackedDataListeners.remove(listener);
-	}
-	
-
-	private void listenToTrackedData() {
-		serviceConnection.registerListener(new TrackedDataListener() {
-			@Override
-			public void trackData(TrackedDataItem dataItem) {
-				processTrackedData(dataItem);	
-			}
-		});
-	}
-
-	private void processTrackedData(TrackedDataItem dataItem) {
-		if (state != TrainingState.RUNNING)
+		if (state == TrainingState.FINISHED) 
 			return;
 		
-		notifyTrackedDataListerners(dataItem);
-		//databaseAdapter.saveTrackedDataItem(trainingUnit.getSessionID(), dataItem);
+		state = TrainingState.FINISHED;
+		tracker.unregisterListener(dataListener);
 	}
 
-	private void notifyTrackedDataListerners(TrackedDataItem dataItem) {
-		for (TrackedDataListener listener : trackedDataListeners)
-			listener.trackData(dataItem);
+	/**
+	 * Registriert den listener, so dass dieser über eingehende 
+	 * {@link TrainingDetail}s informiert wird.
+	 * @param listener Der {@link TrainingDetailListener}.
+	 */
+	public void registerListener(TrainingDetailListener listener) {
+		if (listener == null)
+			throw new IllegalArgumentException("listener cannot be null");
+
+		if (!trainingDetailListeners.contains(listener))
+			trainingDetailListeners.add(listener);
 	}
 
-	// /**
-	// * @return Liefert den insgesamt gespeicherten Datensatz.
-	// */
-	// public TrainingUnitDetail getStoredData() {
-	// // TODO, Christoph, Chris: Prüfen ob der Datensatz mit der Id noch
-	// existiert,
-	// // da er ja über removeStoredData bereits gelöscht worden sein könnte...
-	// return
-	// databaseAdapter.getTrainingUnitDetail(trainingUnit.getSessionID());
-	// }
-	//
-	// /**
-	// * Verwerfe diese Trainingseinheit wieder.
-	// */
-	// public void removeStoredData() {
-	// // TODO Christoph, Chris: Methode zum Löschen programmieren.
-	// }
+	/**
+	 * Entfernt einen registrierten listener wieder.
+	 * @param listener Der registrierte {@link TrainingDetailListener}.
+	 */
+	public void unregisterTrackedDataListener(TrainingDetailListener listener) {
+		if (listener == null)
+			throw new IllegalArgumentException("listener cannot be null");
 
-	// Dadurch das wir die ganze Zeit speichern wird save
-	// eigentlich überflüssig.
-	// public void save();
+		if (trainingDetailListeners.contains(listener))
+			trainingDetailListeners.remove(listener);
+	}
 
-	// Forget wird nötig, damit die Einträge wieder aus der
-	// Datenbank gelöscht werden, sofern man die Trainingseinheit
-	// nicht speichern sondern wieder verwerfen möchte.
-	// public void forget();
+	private void initializeDataListener() {
+		dataListener = new TrackedDataListener() {
+			@Override
+			public void trackData(TrackedDataItem dataItem) {
+				assert dataItem != null : "dateItem cannot be null";
+				if (state == TrainingState.RUNNING)
+					processTrackedData(dataItem);
+			}
+		};
+	}
 
-	// public void pause() {
-	// throw new
-	// UnsupportedOperationException("pause() is not implemented yet.");
-	// }
-	//
-	// public void proceed() {
-	// throw new
-	// UnsupportedOperationException("proceed() is not implemented yet.");
-	// }
+	private void processTrackedData(TrackedDataItem dataItem) {		
+		TrainingDetail detail = createTrainingDetail(dataItem);
+
+		notifyTrainingDetailListeners(detail);
+		database.saveTrainingDetail(currentTraining.getTrainingUnitId(), detail);
+	}
+	
+	private TrainingDetail createTrainingDetail(TrackedDataItem dataItem) {
+		TrainingDetail detail = new TrainingDetail();
+		
+		// TODO korrekt umrechnen!
+		detail.setDistanceInMeter((int)dataItem.getDistance());
+		detail.setHeartRate((int)dataItem.getHeartRate());
+		detail.setNumberOfStrides((int)dataItem.getStrides());
+		detail.setSpeedInMeterPerSecond((int)dataItem.getSpeed());
+		
+		return detail;
+	}
+
+	private void notifyTrainingDetailListeners(TrainingDetail detail) {
+		for (TrainingDetailListener listener : trainingDetailListeners)
+			listener.trackTrainingDetail(detail);
+	}
 }
