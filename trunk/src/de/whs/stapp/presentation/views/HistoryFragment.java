719fca737ea5be9708248556ddad42ec1f7e9aee
thomas.buning@gmail.com <thomas.buning@gmail.com@df7c1e32-7cd3-0938-7b03-7a48d7c20f59>
2013-04-23 11:50:48 +0000

    Swipe in die Pages integriert.

--- trunk/src/de/whs/stapp/presentation/views/HistoryFragment.java
+++ trunk/src/de/whs/stapp/presentation/views/HistoryFragment.java
@@ -4,7 +4,7 @@
 package de.whs.stapp.presentation.views;
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
