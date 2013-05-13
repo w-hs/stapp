96d2f6cd5fba5f236ea52ce42c20c0e70b3e57e9
paus.fabian@gmail.com <paus.fabian@gmail.com@df7c1e32-7cd3-0938-7b03-7a48d7c20f59>
2013-05-13 11:43:27 +0000

    Testfälle für die Generierung von Javascript-Aufrufen angepasst

--- src/de/whs/stapp/test/presentation/JavascriptTest.java
+++ src/de/whs/stapp/test/presentation/JavascriptTest.java
@@ -1,11 +1,9 @@
 package de.whs.stapp.test.presentation;
 
-import java.math.BigDecimal;
-
-import de.whs.stapp.presentation.viewmodels.TrainingSession;
-import de.whs.stapp.presentation.Javascript;
 import junit.framework.Assert;
 import junit.framework.TestCase;
+import de.whs.stapp.presentation.Javascript;
+import de.whs.stapp.presentation.viewmodels.TrainingSession;
 
 /**
  * Testet die Erzeugung von Javascript-Funktionsaufrufen.
@@ -18,10 +16,8 @@ public class JavascriptTest extends TestCase {
 	 * Testet Funktionen ohne Argumente.
 	 */
 	public void testNoArguments() {
-
-		Assert.assertEquals("javascript:test()", Javascript.getFunctionCall("test"));
-		
-		Assert.assertEquals("javascript:stapp.foo()", Javascript.getFunctionCall("stapp.foo"));
+		Assert.assertEquals("javascript:test('')", Javascript.getFunctionCall("test"));
+		Assert.assertEquals("javascript:stapp.foo('')", Javascript.getFunctionCall("stapp.foo"));
 	}
 	
 	/**
@@ -30,7 +26,7 @@ public class JavascriptTest extends TestCase {
 	public void testWithArguments() {
 		
 		TrainingSession session = new TrainingSession(5200, 80);
-		Assert.assertEquals("javascript:test({\"distance\":\"5200\",\"heartfrequence\":\"80\"})", 
+		Assert.assertEquals("javascript:test('{\"distance\":\"5200\",\"heartfrequence\":\"80\"}')", 
 				Javascript.getFunctionCall("test", session));
 	}
 }
