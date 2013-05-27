372f3f4359ea3b271f899c67ce19691a795e39f9
thomas.buning@gmail.com <thomas.buning@gmail.com@df7c1e32-7cd3-0938-7b03-7a48d7c20f59>
2013-05-27 11:54:40 +0000

    #17 Die Bilder werden runter skaliert.

--- trunk/src/de/whs/stapp/presentation/views/GuidedTourActivity.java
+++ trunk/src/de/whs/stapp/presentation/views/GuidedTourActivity.java
@@ -1,12 +1,16 @@
 package de.whs.stapp.presentation.views;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
@@ -29,6 +33,9 @@ public class GuidedTourActivity extends Activity {
 			R.drawable.gt_7, };
 	private int curIndex = 0;
 
+	private int width = 100;
+	private int height = 100;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -42,7 +49,15 @@ public class GuidedTourActivity extends Activity {
 
 		imageSwitcher = (ImageView) findViewById(R.id.guidedTourImageView);
 
-		loadCurrentImage();
+		ViewTreeObserver vto = imageSwitcher.getViewTreeObserver();
+		vto.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+		    public boolean onPreDraw() {
+		        height = imageSwitcher.getMeasuredHeight()/2;
+		        width = imageSwitcher.getMeasuredWidth()/2;
+				loadCurrentImage();
+		        return true;
+		    }
+		});
 
 		final GestureDetector gdt = new GestureDetector(this,
 				new GestureListener());
@@ -63,7 +78,50 @@ public class GuidedTourActivity extends Activity {
 
 	private void loadCurrentImage() {
 
-		imageSwitcher.setImageResource(imageList[curIndex]);
+		imageSwitcher.setImageBitmap(decodeSampledBitmapFromResource(
+				getResources(), imageList[curIndex], width, height));
+	}
+
+	private int calculateInSampleSize(BitmapFactory.Options options,
+			int reqWidth, int reqHeight) {
+		// Raw height and width of image
+		final int height = options.outHeight;
+		final int width = options.outWidth;
+		int inSampleSize = 1;
+
+		if (height > reqHeight || width > reqWidth) {
+
+			// Calculate ratios of height and width to requested height and
+			// width
+			final int heightRatio = Math.round((float) height
+					/ (float) reqHeight);
+			final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+			// Choose the smallest ratio as inSampleSize value, this will
+			// guarantee
+			// a final image with both dimensions larger than or equal to the
+			// requested height and width.
+			inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+		}
+
+		return inSampleSize;
+	}
+
+	private Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+			int reqWidth, int reqHeight) {
+
+		// First decode with inJustDecodeBounds=true to check dimensions
+		final BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inJustDecodeBounds = true;
+		BitmapFactory.decodeResource(res, resId, options);
+
+		// Calculate inSampleSize
+		options.inSampleSize = calculateInSampleSize(options, reqWidth,
+				reqHeight);
+
+		// Decode bitmap with inSampleSize set
+		options.inJustDecodeBounds = false;
+		return BitmapFactory.decodeResource(res, resId, options);
 	}
 
 	/**
